6fd4dc80eca845968f2b0fb73f9b992f730edcb7
mr-aiza <89704296+mr-aiza@users.noreply.github.com>
2026-07-25 03:42:11 +0330

    Update reyhoon-gallery-worker.js

--- reyhoon-gallery-worker.js
+++ reyhoon-gallery-worker.js
@@ -6,7 +6,8 @@
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Max-Age": "86400",
 };
 
 const PAGE_SIZE = 5;
